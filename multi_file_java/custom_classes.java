f6408eeec00340e5ec0a4e8ae3fa0e3f80e4364f
liu-bruce <87771104+liu-bruce@users.noreply.github.com>
2022-01-19 08:57:59 -0700

    Add files via upload

---
+++ multi_file_java/custom_classes.java
@@ -0,0 +1,11 @@
+package Bruce_Liu_libary;
+
+//must be compiled using javac -d . file_name.java
+public class custom_classes
+{
+  public static void hello_world()
+  {
+    System.out.printf("hello world");
+  }
+
+}
---
+++ multi_file_java/main.java
@@ -0,0 +1,16 @@
+import Bruce_Liu_libary.custom_classes;
+import java.io.*;
+
+
+// compiled after custom_classes
+// javac file_name.java
+// run with java file_name
+public class main
+{
+  public static void main (String [] args)
+  {
+    custom_classes print_test = new custom_classes();
+
+    print_test.hello_world();
+  }
+}
