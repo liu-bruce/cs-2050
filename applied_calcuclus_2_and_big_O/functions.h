a8f18203dc88a59f6b0aa0ebeab9cde33b7de3ee
liu-bruce <87771104+liu-bruce@users.noreply.github.com>
2022-07-10 02:05:11 -0600

    error correction

---
+++ applied_calcuclus_2_and_big_O/functions.h
@@ -0,0 +1,8 @@
+#ifndef functions
+#define functions
+
+
+void fractional_factorial_probalitity(int upper_factorial, int lower_factorial);
+void fractional_factorial_probalitity_natral_log_version(double upper_factorial , double lower_factorial);
+
+#endif
---
+++ applied_calcuclus_2_and_big_O/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "functions.h"
+
+void main()
+{
+fractional_factorial_probalitity(100,90);
+fractional_factorial_probalitity_natral_log_version(100.0,90.0);
+}
